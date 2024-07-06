18dc5eebc1bc002e828acc05e562ac989417725e
sou-rav3 <mdsourav54u@gmail.com>
2024-07-06 20:32:18 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp()); //Application
+void main() {
+  runApp(const IntroApp());
 }
 
-
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class IntroApp extends StatelessWidget {
+  const IntroApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      title: 'eFruits App'
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('eSports App'),
-        backgroundColor: Colors.blue,
+        title: Text('eFruits App'),
       ),
-        drawer: Drawer(
-          width: 205,
-          child: ListView(
-            children: [
-              DrawerHeader(child: Text('Menu',
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.black87
-                ),)),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
-              ),
-              ListTile(
-                leading: Icon(Icons.contact_emergency),
-                title: Text('Contact'),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_box),
-                title: Text('Account'),
-              )
-            ],
-          ),
-        ),
 
+      body: Center(
+        // child: Icon(Icons.home,
+        // size: 90,
+        // color: Colors.blue,),
+        child: Image.asset('assets/images/staw.png'),
 
-
-
-
-        body: Center(
-          child: Text('Welcome to eSports era',
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            fontSize: 32,
-          ),
-          ),
-
-        ),
       ),
     );
   }
-
 }
