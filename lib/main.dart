077f2b4645e37135fbbb9bd696716b044b32b77b
sou-rav3 <mdsourav54u@gmail.com>
2024-07-06 22:23:48 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const IntroApp());
+  runApp(const FirstApp());
 }
 
-class IntroApp extends StatelessWidget {
-  const IntroApp({super.key});
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-      title: 'eFruits App'
-    );
+        debugShowCheckedModeBanner: false, home: Home(), title: 'eFruits App');
   }
 }
 
@@ -24,15 +21,66 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('eFruits App'),
+        title: Text('eFruits App', style: TextStyle(
+          fontSize: 30,
+        ),),
       ),
-
+      drawer: Drawer(
+        width: 205,
+        backgroundColor: Colors.lightBlueAccent,
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child: Text(
+              'Menu',
+              style: TextStyle(
+                fontSize: 45,
+              ),
+            )),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                size: 35,
+              ),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person,
+                size: 35,
+              ),
+              title: Text(
+                'Profile',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_box,
+                size: 35,
+              ),
+              title: Text(
+                'Account',
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
       body: Center(
         // child: Icon(Icons.home,
         // size: 90,
         // color: Colors.blue,),
         child: Image.asset('assets/images/staw.png'),
-
       ),
     );
   }
